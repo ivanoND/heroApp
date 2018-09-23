e68a7883bc842ceeafed5c89ec528037a50015cb
ivano di nicola <i.dinicola@nesea.it>
2018-09-23 19:34:08 +0200

    added the CrossOrigin annotation

--- src/main/java/it/nesea/epj/backend/controllers/RestController.java
+++ src/main/java/it/nesea/epj/backend/controllers/RestController.java
@@ -2,17 +2,13 @@ package it.nesea.epj.backend.controllers;
 
 import it.nesea.epj.backend.entities.Hero;
 import it.nesea.epj.backend.services.HeroService;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.validation.Valid;
+import java.util.List;
 
+@CrossOrigin("http://localhost:4200")
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
@@ -20,22 +16,7 @@ public class RestController {
     private HeroService heroService;
 
     @GetMapping("/api/heroes")
-    public JsonResponseBody getHeroes(){
-            return new JsonResponseBody(HttpStatus.OK,heroService.getHeroes());
-    }
-
-    @GetMapping("/api/hero/{name}")
-    public JsonResponseBody insertHero(@PathVariable("name") @Valid String name){
-        return new JsonResponseBody(HttpStatus.OK,heroService.addHero(new Hero(name)));
-    }
-
-    @AllArgsConstructor
-    private class JsonResponseBody{
-
-        @Getter @Setter
-        private HttpStatus status;
-
-        @Getter @Setter
-        private Object response;
+        public List<Hero> getHeroes(){
+            return heroService.getHeroes();
     }
 }
