5cc54110c9e56f9d00844dc7ed7beab3eb4260ff
i.dinicola <i.dinicola@nesea.it>
2018-10-11 14:45:17 +0200

    changed the return of AbilityController's method in ResponseEntity

--- src/main/java/it/nesea/epj/backend/controllers/AbilityRestController.java
+++ src/main/java/it/nesea/epj/backend/controllers/AbilityRestController.java
@@ -5,9 +5,11 @@ import it.nesea.epj.backend.entities.Hero;
 import it.nesea.epj.backend.services.AbilityService;
 import it.nesea.epj.backend.services.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.xml.ws.Response;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -22,36 +24,40 @@ public class AbilityRestController {
         this.abilityService=abilityService;
     }
     @GetMapping("/api/abilities")
-    public List<Ability> getAbilities(){
-        return abilityService.getAbilities();
+    public ResponseEntity getAbilities(){
+        return ResponseEntity.ok(this.abilityService.getAbilities());
     }
 
     @GetMapping("/api/abilities/{heroId}")
-    public List<Ability> getNoHeroAbilities(@Valid @PathVariable("heroId") long heroId){
-        List<Ability> allAbilities = getAbilities();
-        List<Ability> heroAbilities = abilityService.getHeroAbilities(heroId);
+    public ResponseEntity getNoHeroAbilities(@Valid @PathVariable("heroId") long heroId){
+        List<Ability> allAbilities = (List<Ability>) getAbilities().getBody();
+        List<Ability> heroAbilities = this.abilityService.getHeroAbilities(heroId);
 
         allAbilities.removeAll(heroAbilities);
-        return allAbilities;
+        return ResponseEntity.ok(allAbilities);
     }
 
     @GetMapping("/api/ability/{id}")
-    public Ability getAbility(@Valid @PathVariable("id") Long id){
-        return abilityService.getAbilityById(id);
+    public ResponseEntity getAbility(@Valid @PathVariable("id") Long id){
+        Ability ability = this.abilityService.getAbilityById(id);
+        if(ability==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ability);
     }
 
     @PutMapping("/api/ability")
-    public Ability updateAbility(@Valid @RequestBody Ability ability) {
-        return abilityService.updateAbility(ability);
+    public ResponseEntity updateAbility(@Valid @RequestBody Ability ability) {
+        return ResponseEntity.ok(this.abilityService.updateAbility(ability));
     }
 
     @DeleteMapping("/api/ability/{id}")
-    public Ability deleteAbility(@Valid @PathVariable("id") long id){
-        return abilityService.deleteAbilityById(id);
+    public ResponseEntity deleteAbility(@Valid @PathVariable("id") long id){
+        return ResponseEntity.ok(this.abilityService.deleteAbilityById(id));
     }
 
     @PostMapping("/api/ability")
-    public Ability addAbility(@Valid @RequestBody Ability ability){
-        return this.abilityService.addAbility(ability);
+    public ResponseEntity addAbility(@Valid @RequestBody Ability ability){
+        return ResponseEntity.ok(this.abilityService.addAbility(ability));
     }
 }
--- src/main/java/it/nesea/epj/backend/controllers/HeroRestController.java
+++ src/main/java/it/nesea/epj/backend/controllers/HeroRestController.java
@@ -25,23 +25,23 @@ public class HeroRestController {
 
     @GetMapping("/api/heroes")
     public ResponseEntity getHeroes() {
-        return ResponseEntity.ok(heroService.getHeroes());
+        return ResponseEntity.ok(this.heroService.getHeroes());
     }
 
     @PostMapping("/api/hero")
     public ResponseEntity addHero(@Valid @RequestBody Hero hero) {
-        return ResponseEntity.ok(heroService.addHero(hero));
+        return ResponseEntity.ok(this.heroService.addHero(hero));
     }
 
     @DeleteMapping("/api/hero/{id}")
     public ResponseEntity deletHero(@Valid @PathVariable("id") long id) {
-        return ResponseEntity.ok(heroService.deleteHeroById(id));
+        return ResponseEntity.ok(this.heroService.deleteHeroById(id));
     }
 
     @GetMapping("/api/hero/{id}")
     public ResponseEntity getHero(@Valid @PathVariable("id") Long id) {
 
-        Hero hero = heroService.getHeroById(id);
+        Hero hero = this.heroService.getHeroById(id);
         if(hero==null){
             return ResponseEntity.notFound().build();
         }
@@ -50,12 +50,12 @@ public class HeroRestController {
 
     @PutMapping("/api/hero")
     public ResponseEntity updateHero(@Valid @RequestBody Hero hero) {
-        return ResponseEntity.ok(heroService.updateHero(hero));
+        return ResponseEntity.ok(this.heroService.updateHero(hero));
     }
 
     @GetMapping("/api/hero/name")
     public ResponseEntity searchHeroes(@Valid @RequestParam("name") String name) {
-        return ResponseEntity.ok(heroService.getHeroes()
+        return ResponseEntity.ok(this.heroService.getHeroes()
                 .stream()
                 .filter(hero -> hero.getName().contains(name))
                 .collect(Collectors.toList()));
